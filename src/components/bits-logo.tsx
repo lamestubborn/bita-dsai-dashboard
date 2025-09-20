38f35342888c49ca786fe67f06fca8814ad4a518
Aanand <aanandarora.mba@gmail.com>
2025-09-20 22:52:14 +0000

    you also add white behind logo otherwise logo is not visible in dark mod

--- src/components/bits-logo.tsx
+++ src/components/bits-logo.tsx
@@ -3,7 +3,7 @@ import { cn } from '@/lib/utils';
 
 export function BITSLogo({ className }: { className?: string }) {
   return (
-    <div className={cn('rounded-sm p-1', className)}>
+    <div className={cn('rounded-sm p-1 dark:bg-white', className)}>
       <svg
         viewBox="0 0 250 55"
         fill="none"
