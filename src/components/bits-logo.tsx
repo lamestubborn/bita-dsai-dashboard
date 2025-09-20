bd165b08859d3e84f3123b6a6dc3253c4e066e24
Aanand <aanandarora.mba@gmail.com>
2025-09-20 22:12:44 +0000

    use logo from here https://bitspilani-digital.edu.in/assets/images/logo.

--- src/components/bits-logo.tsx
+++ src/components/bits-logo.tsx
@@ -4,19 +4,17 @@ export function BITSLogo({ className }: { className?: string }) {
   return (
     <svg
       className={className}
-      viewBox="0 0 330 80"
+      viewBox="0 0 250 55"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      xmlnsXlink="http://www.w3.org/1999/xlink"
       role="img"
       aria-labelledby="bits-logo-title"
     >
       <title id="bits-logo-title">BITS Pilani Logo</title>
       <image
-        href="https://upload.wikimedia.org/wikipedia/en/thumb/d/d3/BITS_Pilani-Logo.svg/1200px-BITS_Pilani-Logo.svg.png"
-        height="80"
-        width="330"
-        
+        href="https://bitspilani-digital.edu.in/assets/images/logo.svg?v=1758387037"
+        height="55"
+        width="250"
       />
     </svg>
   );
